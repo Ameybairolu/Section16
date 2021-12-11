44e9d340fd6c245ebdb5d7aecaeffca98d027457
Ameybairolu <ameybairolu@gmail.com>
2021-12-11 13:28:12 +0530

    Added style when invalid input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -6,27 +6,39 @@ const SimpleInput = (props) => {
 
   const [enteredName, setEnteredName] = useState('');
 
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
   };
 
   const formSubmissionHandler = event => {
     event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+        {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
-    </form>
+    </form >
   );
 };
 
